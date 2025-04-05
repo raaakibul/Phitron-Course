ba58d69463671be1cc0ba2d462af5b44e790b2eb
rakibul-islam <raakibul.uap@gmail.com>
2025-04-05 22:46:57 +0600

    module 1 code

---
+++ Intro-to-Programming-Language/Module-1/intro.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main(){
+    printf("Hello World\n");
+    int num= 10;
+    printf("%d\n", num);
+    char ch= 'A';
+    printf("%c\n", ch);
+
+    float f= 10.5;
+    printf("%f\n", f);
+    double d= 10.5;
+    printf("%lf\n", d);
+    int a= 10, b= 20;
+    printf("%d\n", a+b);
+
+    bool isTrue= true;
+    printf("%d\n", isTrue);
+}
