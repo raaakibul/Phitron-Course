71951790aee034900d97a5623cf1e1538ab6f6ea
rakibul-islam <raakibul.uap@gmail.com>
2025-04-06 20:46:30 +0600

    arithmetic from module 2

---
+++ Intro-to-Programming-Language/Module-2/arithmetic.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a = 10;
+    int b = 5;
+    printf("%d\n", a+b); // Addition
+    printf("%d\n", a-b); // Subtraction
+    printf("%d\n", a*b); // Multiplication
+    printf("%d\n", a/b); // Division
+    printf("%d\n", a%b); // Modulus
+    printf("%d\n", a++); // Post-increment
+    printf("%d\n", ++a); // Pre-increment
+    printf("%d\n", a--); // Post-decrement
+    printf("%d\n", --a); // Pre-decrement
+}
