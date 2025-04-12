e96d0f5d38f7fd51f7e8e1dfe6a55727ab0aac6c
rakibul-islam <raakibul.uap@gmail.com>
2025-04-12 21:05:02 +0600

    problem solving

--- Intro-to-Programming-Language/Module-5/problem-3.c
+++ Intro-to-Programming-Language/Module-5/problem-3.c
@@ -2,5 +2,11 @@
 int main(){
     int n;
     scanf("%d", &n);
-    if(n)
+    int first_digit = n/1000;
+    if(first_digit % 2==0){
+        printf("EVEN");
+    }else{
+        printf("ODD");
+    }
+    return 0;
 }
