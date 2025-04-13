c904f22ed03c1b4a223ea1642e8a9e5bba80a36d
rakibul-islam <raakibul.uap@gmail.com>
2025-04-13 23:09:55 +0600

    Module 6 started

--- Intro-to-Programming-Language/Module-6/increment.c
+++ Intro-to-Programming-Language/Module-6/increment.c
@@ -2,5 +2,19 @@
 int main(){
     int i = 10;
     i*=3;
-    printf("%d",i);
+    printf("%d\n",i);
+
+    // post increment 
+    for(int i =1;i<=5;i++){
+        printf("%d ",i);
+    }
+    // pre increment 
+    for(int i=1;i<=5;++i){
+        printf("%d ",i);
+    }
+
+    int x = 10;
+    int y = x++;
+    printf("%d %d", x,y);
+
 }
