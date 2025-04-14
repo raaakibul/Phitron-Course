c3d1a3370c3459529fcfaa0502251b3557c6143b
rakibul-islam <raakibul.uap@gmail.com>
2025-04-14 13:29:33 +0600

    problem solving

---
+++ Intro-to-Programming-Language/Module-6/Even_odd_positive_negative.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n,x;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        scanf("%d",&x);
+    }
+
+    for(int i=1;i<=n;i++){
+        printf("%d\n",x);
+    }
+
+    
+    return 0;
+}
