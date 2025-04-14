397bd1e1e50b7760d4049f10b82c091884ac89b1
rakibul-islam <raakibul.uap@gmail.com>
2025-04-14 13:41:52 +0600

    problem solving

--- Intro-to-Programming-Language/Module-6/Even_odd_positive_negative.c
+++ Intro-to-Programming-Language/Module-6/Even_odd_positive_negative.c
@@ -2,14 +2,26 @@
 int main(){
     int n,x;
     scanf("%d",&n);
+    int even =0, odd=0, negative=0, positive=0;
     for(int i=1;i<=n;i++){
         scanf("%d",&x);
-    }
-
-    for(int i=1;i<=n;i++){
-        printf("%d\n",x);
-    }
-
-    
+        if(x%2==0){
+            even++;
+        }
+        else{
+            odd++;
+        }
+        if(x>0){
+            positive++;
+        }
+        else if(x<0){
+            negative++;
+        }
+     }
+     
+     printf("Even: %d\n",even);
+     printf("Odd: %d\n",odd);
+     printf("Positive: %d\n",positive);
+     printf("Negative: %d\n",negative);
     return 0;
 }
