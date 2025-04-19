8443a975a193d26055bbcd14352c36e68250bb39
rakibul-islam <raakibul.uap@gmail.com>
2025-04-19 15:51:28 +0600

    array input output

---
+++ Intro-to-Programming-Language/Module-7/Array_input_output.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int array[n];
+
+    for(int i=0;i<n;i++){
+        scanf("%d",&array[i]);
+    }
+    for(int i=0;i<n;i++){
+        printf("%d\n",array[i]);
+    }
+}
