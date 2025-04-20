b9fb1ce3f6c03d5fb2aadc1bdca63117a892d792
rakibul-islam <raakibul.uap@gmail.com>
2025-04-20 19:05:40 +0600

    delete

--- Intro-to-Programming-Language/Module-9/insert.c
+++ Intro-to-Programming-Language/Module-9/insert.c
@@ -2,7 +2,7 @@
 int main(){
     int n;
     scanf("%d",&n);
-    int array[n];
+    int array[n+1];
     for(int i=0;i<n;i++){
         scanf("%d",array[i]);
     }
@@ -12,6 +12,7 @@ int main(){
     for(int i=n;i>=index+1;i--){
         array[i]=array[i-1];
     }
+    array[index] = index_value;
 
     for(int i=0;i<=n;i++){
         printf("%d ",array[i]);
