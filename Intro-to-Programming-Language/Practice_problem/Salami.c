a3abba0b71c254bb4817050b58ae581d52a87789
rakibul-islam <raakibul.uap@gmail.com>
2025-04-28 20:07:23 +0600

    problem solving

---
+++ Intro-to-Programming-Language/Practice_problem/Salami.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int Abul, Babul,Bablu;
+    scanf("%d %d %d",&Abul, &Babul, &Bablu);
+    int total = Abul + Babul +Bablu;
+    printf("%d",total);
+    return 0;
+}
