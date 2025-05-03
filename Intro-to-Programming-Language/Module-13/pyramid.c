b6165d9ff63c851db932f639437c8bb63f54353c
rakibul-islam <raakibul.uap@gmail.com>
2025-05-03 20:39:12 +0600

    Pattern code

---
+++ Intro-to-Programming-Language/Module-13/pyramid.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int space = n-1;
+    int star =1;
+    for(int i=1;i<=n;i++){
+        for(int k=1;k<=space;k++){
+            printf(" ");
+        }
+        for(int j=1;j<=star;j++){
+            printf("*");
+        }
+        printf("\n");
+        star +=2;
+        space--;
+    }
+}
