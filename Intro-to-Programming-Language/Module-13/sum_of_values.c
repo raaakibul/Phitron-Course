38733decedc5b7ea25eb695fa86956c1dee3539d
rakibul-islam <raakibul.uap@gmail.com>
2025-05-04 15:27:09 +0600

    flag variable

--- Intro-to-Programming-Language/Module-13/sum_of_values.c
+++ Intro-to-Programming-Language/Module-13/sum_of_values.c
@@ -10,16 +10,20 @@ int main(){
 
     int x;
     scanf("%d",&x);
+    int flag=0;
+
     for(int i=0;i<n-1;i++){
         for(int j=i+1;j<n;j++){
             // printf("%d %d\n",array[i],array[j]);
             if(array[i] + array[j]==x){
-                printf("Yes\n");
-            }
-            else{
-                printf("No\n");
+                flag =1;
+                // printf("Yes\n");
+                printf("%d %d", array[i],array[j]);
             }
         }
+    }
+    if(flag==0){
+        printf("No\n");
     }
     return 0;
 }
