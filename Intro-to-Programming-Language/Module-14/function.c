b6b5e80a37f08ab70ae9680df8b345b4db235e4d
rakibul-islam <raakibul.uap@gmail.com>
2025-05-04 20:19:45 +0600

    function

--- Intro-to-Programming-Language/Module-14/function.c
+++ Intro-to-Programming-Language/Module-14/function.c
@@ -4,9 +4,14 @@ int sum(int num1, int num2){
     // printf("%d", result);
     return result;
 }
+int subtraction(int num1, int num2){
+    int result = num1 - num2;
+    return result;
+}
 int main(){
-    int ans = sum(10,20);
-
-    printf("%d", ans);
+    int add = sum(10,20);
+    int sub = subtraction(20,10);
+    printf("%d", add);
+    printf("%d", sub);
     return 0;
 }
