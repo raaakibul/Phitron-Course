9c45fc253e1b03861625914256945e9051b118b1
rakibul-islam <raakibul.uap@gmail.com>
2025-05-18 22:15:16 +0600

    recursion

---
+++ Intro-to-Programming-Language/Module-19/char_recursion.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void recursion(char ch[],int i)
+{
+    if (ch[i]=='\0') 
+    return;
+    printf("%c ",ch[i]);
+    recursion(ch,i+1);
+}
+
+int main()
+{
+    int test_case;
+    scanf("%d",&test_case);
+    for (int i=0;i<test_case;i++)
+    {
+    char ch[12];
+    scanf("%s",ch);
+    recursion(ch,0);
+    printf("\n");
+    }
+
+    return 0;
+}
