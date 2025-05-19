6844872cec5a576b4f96735984894306dc4e99f5
rakibul-islam <raakibul.uap@gmail.com>
2025-05-19 22:56:47 +0600

    void function

---
+++ Intro-to-Programming-Language/Module-14/just_function call.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void sum(void){
+
+    int x,y;
+    scanf("%d %d",&x,&y);
+    int s=x+y;
+    printf("%d\n",s);
+
+}
+
+int main(){
+    
+    sum();
+    sum();
+    sum();
+    return 0;
+}
