c0b43ceefdc26bc67abd2393a8104930f0896385
rakibul-islam <raakibul.uap@gmail.com>
2025-05-22 22:57:51 +0600

    DSA folder added

---
+++ DSA/Module-1/intro.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int array[n];
+    for (int i=0;i<n;i++){
+        cin>>array[i];
+        cout<<array[i]<<" ";
+    }
+}
