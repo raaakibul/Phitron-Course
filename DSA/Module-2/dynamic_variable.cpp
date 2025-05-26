5d1280178122b684a8adc87fab07b959c443cca0
rakibul-islam <raakibul.uap@gmail.com>
2025-05-26 13:35:38 +0600

    dynamic variable

---
+++ DSA/Module-2/dynamic_variable.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int *p;
+void fun(){
+    int x =10;
+    return;
+}
+int main(){
+
+    // int x =100;
+    // cout << x <<endl;
+
+    // int *p = new int;
+    // *p = 10;
+    // cout << *p << endl;
+    
+    fun();
+
+    return 0;
+}
