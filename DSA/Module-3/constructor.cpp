b0e926aafd3d2bd4c21b1774f02b6fa51d01c7b9
rakibul-islam <raakibul.uap@gmail.com>
2025-05-28 22:54:29 +0600

    constructor

--- DSA/Module-3/constructor.cpp
+++ DSA/Module-3/constructor.cpp
@@ -15,14 +15,17 @@ class Student{
 
 };
 int main(){
-    Student s1(45,5,3.2);
-    Student s2(10,5,4.32);
-
+    // Student s1(45,5,3.2);
+    // Student s2(10,5,4.32);
+    int r, c;
+    double g;
+    cin >> r >> c >> g;
+    Student s1(r, c, g);
+    Student s2(r,c,g);
 
     cout << s1.roll << " " << s1.cls << " " << s1.gpa << endl;
     cout << s2.roll << " " << s2.cls << " "
     << s2.gpa << endl;
-
 
     return 0;
 }
