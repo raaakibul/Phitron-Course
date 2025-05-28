5458b7fd06bbe3ef94891cfebfadff88a16473ed
rakibul-islam <raakibul.uap@gmail.com>
2025-05-28 22:43:47 +0600

    class and object

--- DSA/Module-3/class_object.cpp
+++ DSA/Module-3/class_object.cpp
@@ -10,13 +10,21 @@ class Student{
 
 int main(){
     // create object 
-    Student s1;
-    s1.roll = 10;
-    s1.gpa = 4.5; 
-    char temp[100] = "Abrar";
-    strcpy(s1.name,temp);
+    Student s1, s2;
+    // s1.roll = 10;
+    // s1.gpa = 4.5; 
+    // char temp[100] = "Abrar";
+    // strcpy(s1.name,temp);
+
+    cin.getline(s1.name, 100);
+    cin >> s1.roll >> s1.gpa;
+    cin.ignore();
+
+    cin.getline(s2.name,100);
+    cin >> s2.name >> s2.roll >> s2.gpa;
     
     cout << s1.name << " "<< s1.roll << " " << s1.gpa <<endl;
+    cout << s2.name << " "<< s2.roll << " " << s2.gpa <<endl;
 
     return 0;
 }
