e2b47f024e9385b3911acd3be218b91381ca5ff1
rakibul-islam <raakibul.uap@gmail.com>
2025-06-01 17:19:30 +0600

    dynamic array

--- DSA/Module-2/dynamic_array_size.cpp
+++ DSA/Module-2/dynamic_array_size.cpp
@@ -2,18 +2,19 @@
 using namespace std;
 int main(){
 
-    int a[3];
+    int *a = new int[3];
     for(int i=0;i<3;i++){
         cin >> a[i];
     }
 
-    int b[5];
+    int *b = new int[5];
     for(int i=0;i<3;i++){
         b[i] = a[i];
     }
 
     b[3] = 40;
     b[4] = 50;
+    delete[] a; //array(a) delete
     for(int i=0;i<5;i++){
         cout << b[i] << " ";
     }
