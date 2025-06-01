f56c6674f690a8b91390ca92cf635768cb72d9f3
rakibul-islam <raakibul.uap@gmail.com>
2025-06-01 17:16:13 +0600

    dynamic array

--- DSA/Module-2/dynamic_array_return_function.cpp
+++ DSA/Module-2/dynamic_array_return_function.cpp
@@ -1,8 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 int* fun(){
-    // int a[5];
-    int *a = new int[5];
+    // int a[5]; // if we return the value it will show segmentation fault
+    int *a = new int[5]; // dynamic array
     for(int i=0;i<5;i++){
         cin >> a[i];
     }
