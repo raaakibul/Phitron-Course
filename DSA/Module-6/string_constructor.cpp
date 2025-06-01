ed51b3c17b1f0847b728f4d1465f924566447531
rakibul-islam <raakibul.uap@gmail.com>
2025-06-01 22:31:56 +0600

    module 6 added

--- DSA/Module-6/string_constructor.cpp
+++ DSA/Module-6/string_constructor.cpp
@@ -7,8 +7,9 @@ int main(){
     // 2
     // string s("Hello world",4);
     // 3
-    string s = "Hello world";
-    string t(s,4);
-    cout << t << endl;
+    // string s = "Hello world";
+    // string t(s,4);
+    string s(5,'A');
+    cout << s << endl;
     return 0;
 }
