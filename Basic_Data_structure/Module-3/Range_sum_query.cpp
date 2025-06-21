5b5c17b4a11f0fd768b164f555635c49030a45c4
rakibul-islam <raakibul.uap@gmail.com>
2025-06-21 22:20:23 +0600

    module 3 added

--- Basic_Data_structure/Module-3/Range_sum_query.cpp
+++ Basic_Data_structure/Module-3/Range_sum_query.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    // bruteforce way
     int n,q;
     cin >> n >> q;
     vector<int>v(n+1);
@@ -8,14 +9,31 @@ int main(){
     for(int i=1;i<=n;i++){
         cin >> v[i];
     }
+    // prefix sum 
+    vector<int>pre(n+1);
+    pre[1]=v[1];
+    for(int i=2;i<=n;i++){
+        pre[i] = pre[i-1]+v[i];
+    }
+    for(int i=1;i<=n;i++){
+        cout << pre[i]<< " ";
+    }
     while(q--){
         int l,r;
         cin >> l>> r;
-        int sum =0;
-        for(int i=l;i<=r;i++){
-            sum+=v[i];
+        int sum;
+        if(l==1){
+            sum = pre[r];
         }
-        cout << sum << endl;
+        else{
+            sum = pre[r]-pre[l-1];
+        }
+        cout << sum <<endl;
+
+        // for(int i=l;i<=r;i++){
+        //     sum+=v[i];
+        // }
+        // cout << sum << endl;
     }
     return 0;
 }
