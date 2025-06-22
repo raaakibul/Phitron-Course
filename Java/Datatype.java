67c1109caa551060d4d9f1aa5ee834b200275765
rakibul-islam <raakibul.uap@gmail.com>
2025-06-22 22:46:18 +0600

    java code

---
+++ Java/Datatype.java
@@ -0,0 +1,12 @@
+public class Datatype {
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println(i);
+        float f = 10.5f;
+        System.out.println(f);
+        boolean b = true;
+        System.out.println(b);
+        double d = 10.236648788;
+        System.out.println(d);
+    }
+}
---
+++ Java/Intro.java
@@ -0,0 +1,5 @@
+class Intro{
+    public static void main(String[] a){
+        System.out.println("Intro to java");
+    }
+}
