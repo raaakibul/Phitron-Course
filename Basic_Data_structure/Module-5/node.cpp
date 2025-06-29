b501cc5db9407e3edcacc3cb8752c9cd6c631341
rakibul-islam <raakibul.uap@gmail.com>
2025-06-29 15:06:36 +0600

    node constructor

--- Basic_Data_structure/Module-5/node.cpp
+++ Basic_Data_structure/Module-5/node.cpp
@@ -8,15 +8,20 @@ class Node{
 };
 
 int main(){
-    Node a,b;
+    Node a,b,c;
     a.val = 10;
     b.val = 20;
+    c.val = 30;
 
-    a.next=&b;
-    b.next = NULL;
+    a.next = &b;
+    // b.next = NULL;
+    b.next = &c;
+    c.next = NULL;
 
     cout << a.val << endl;
     cout << a.next -> val << endl;
+    cout << b.next -> val << endl;
+    cout << c.next -> val << endl;
 
     return 0;
 }
