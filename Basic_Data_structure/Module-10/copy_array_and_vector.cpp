438289c8c6ae5a63e9e834888a14caacbbb26b42
rakibul-islam <raakibul.uap@gmail.com>
2025-07-08 22:55:41 +0600

    module 10 added

---
+++ Basic_Data_structure/Module-10/copy_array_and_vector.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    vector<int>v={10,20,30,40,50};
+    list<int>MyList(v.begin(),v.end());
+
+    cout<<"Print From Itarator: ";
+    for(auto it=MyList.begin();it!=MyList.end();it++){
+        cout<<*it<<" ";
+    }
+    cout<<endl;
+    cout<<"Print From Range Based Loop: ";
+    for(int vall:MyList){
+        cout<<vall<<" ";
+    }
+    return 0;
+}
