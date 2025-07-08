a74e22c0ab5fdd4743e81b17c3fc415d97687cf4
rakibul-islam <raakibul.uap@gmail.com>
2025-07-08 22:57:51 +0600

    stl code

--- Basic_Data_structure/Module-10/copy_array_and_vector.cpp
+++ Basic_Data_structure/Module-10/copy_array_and_vector.cpp
@@ -2,17 +2,18 @@
 using namespace std;
 int main(){
 
-    vector<int>v={10,20,30,40,50};
-    list<int>MyList(v.begin(),v.end());
+    vector<int> v={10,20,30,40,50};
+    list<int> MyList(v.begin(),v.end());
 
-    cout<<"Print From Itarator: ";
+    cout << "Print From Itarator: ";
     for(auto it=MyList.begin();it!=MyList.end();it++){
-        cout<<*it<<" ";
+        cout << *it << " ";
     }
-    cout<<endl;
-    cout<<"Print From Range Based Loop: ";
+    cout << endl;
+
+    cout << "Print From Range Based Loop: ";
     for(int vall:MyList){
-        cout<<vall<<" ";
+        cout << vall << " ";
     }
     return 0;
 }
