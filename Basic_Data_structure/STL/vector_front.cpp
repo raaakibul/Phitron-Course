20d56e6934a32e761bf06bf5a39d474982a08a84
rakibul-islam <raakibul.uap@gmail.com>
2025-07-18 22:56:18 +0600

    vector

---
+++ Basic_Data_structure/STL/vector_front.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+
+    for(int i=0;i<v.size();i++){
+        cout << v[i] << " ";
+    }
+    cout << endl;
+    cout << v.front() << endl;
+    cout << v.back() << endl;
+
+    // v.clear();
+    return 0;
+}
