87f68ce38511b9547e979f516a4fbab7a13053e6
rakibul-islam <raakibul.uap@gmail.com>
2025-08-22 21:20:43 +0600

    bfs code

--- Algorithm/Module-2/bfs_level.cpp
+++ Algorithm/Module-2/bfs_level.cpp
@@ -1,43 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void traverse(int source, vector<vector<int>> &adj, vector<bool> &visited){
+void bfs_traversal(int source, vector<vector<int>> &adj, vector<bool> &visited, vector<int> &level) {
     queue<int> q;
     q.push(source);
     visited[source] = true;
+    level[source] = 0;
 
     while(!q.empty()){
         int parent = q.front();
         q.pop();
-        cout << parent << endl;
 
         for(int child : adj[parent]){
-            if(!visited[child]){
+            if (!visited[child]) {
                 q.push(child);
                 visited[child] = true;
+                level[child] = level[parent] + 1;
             }
         }
     }
 }
 
-int main(){
+int main() {
     int node, edge;
     cin >> node >> edge;
 
     vector<vector<int>> adj(node);
     vector<bool> visited(node, false);
+    vector<int> level(node, -1);
 
     while(edge--){
-        int A, B;
-        cin >> A >> B;
-        adj[A].push_back(B);
-        adj[B].push_back(A);
+        int a, b;
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
     }
 
     int source;
     cin >> source;
 
-    traverse(source, adj, visited);
+    bfs_traversal(source, adj, visited, level);
+
+    for(int i = 0; i<node; i++)
+        cout << i << " " << level[i] << endl;
+    
 
     return 0;
 }
