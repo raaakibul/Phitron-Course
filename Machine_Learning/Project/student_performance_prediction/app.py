450786ffa33596b9f2a998acae817fe0237365f0
rakibul-islam <raakibul.uap@gmail.com>
2026-02-01 18:24:02 +0600

    student result prediction

--- Machine_Learning/Project/student_performance_prediction/app.py
+++ Machine_Learning/Project/student_performance_prediction/app.py
@@ -53,6 +53,6 @@ app = gr.Interface(
     fn=predict_gpa,
       inputs=inputs,
         outputs="text", 
-        title="Student HSC Result Predictor")
+        title="Student's HSC Result Predictor")
 
 app.launch(share=True)
